c9d12fa77057c10479ddd8d9e2e903a4ba049a79
Jose <jm_rona@hotmail.com>
2021-04-22 09:59:43 +0100

    Addin CKeditor

--- src/components/ui/Biography/Biography.js
+++ src/components/ui/Biography/Biography.js
@@ -1,19 +1,30 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { startupdatingUser } from '../../../actions/user';
 import { Pencil } from '../Icons/Pencil';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import styles from './Biography.module.css';
 
 export const Biography = () => {
 	const { biography } = useSelector((state) => state.user);
+	const [editing, setEditing] = useState(false);
+	const [value, setValue] = useState('Tell us about you');
+
+	useEffect(() => {
+		setValue(biography);
+	}, [biography]);
+
 	const dispatch = useDispatch();
 	const textAreaRef = useRef();
 
-	const [editing, setEditing] = useState(false);
-
 	const handleEdit = () => {
-		dispatch(startupdatingUser(textAreaRef.current.value));
+		dispatch(startupdatingUser(value));
 		setEditing(false);
+	};
+
+	const handleInputChange = (data) => {
+		setValue(data);
 	};
 
 	return (
@@ -23,12 +34,18 @@ export const Biography = () => {
 				<div className={styles.edit} onClick={() => setEditing(!editing)}>
 					<Pencil width='20px' height='20px' />
 				</div>
-				<textarea
+
+				<CKEditor
+					editor={ClassicEditor}
+					data={value || 'Tell us more about you'}
 					disabled={!editing}
-					name='biograpy'
-					defaultValue={biography}
-					ref={textAreaRef}
-				></textarea>
+					onChange={(event, editor) => {
+						const data = editor.getData();
+						handleInputChange(data);
+					}}
+					columns='auto'
+				></CKEditor>
+
 				{editing && <button onClick={handleEdit}>Update biography</button>}
 			</div>
 		</>
